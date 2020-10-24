0ae54e5ef926947b9f8cc086437d22340abf9d6f
ywangnon <ywangnon123@gmail.com>
2020-10-24 21:52:03 +0900

    tableview overview

--- EssentialTableViewSample/EssentialTableViewSample/ViewController.swift
+++ EssentialTableViewSample/EssentialTableViewSample/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let list = ["iPhone", "iPad", "Apple watch", "iMac Pro", "iMac 5K", "Macbook Pro", "Apple TV"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,3 +19,24 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("#1", #function)
+        return 100
+    }
+    
+    // 최대 16ms 이하에서 처리해야함
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("#2", #function, indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row)"
+        
+        return cell
+    }
+}
